34ab84ca3f06c90172bffa90ac3c088de0483b46
ricaun <ricaun@gmail.com>
2026-01-08 15:31:59 -0300

    Update `Build`

--- Build/Build.cs
+++ Build/Build.cs
@@ -5,7 +5,7 @@ using ricaun.Nuke.Components;
 
 class Build : NukeBuild, IPublishPack, ICompileExample, IBeforeCompile, IPrePack
 {
-    string IHazBeforeCompile.Name => "Isolator.Template";
+    string IHazBeforeCompile.Name => "Isolator.Template*";
     string IHazMainProject.MainName => "Isolator.ConsoleApp";
     string IHazExample.Name => "Isolator";
     public static int Main() => Execute<Build>(x => x.From<IPublishPack>().Build);
